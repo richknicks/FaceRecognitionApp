393590465a0009afbd5a7bc0ea2eb945e7d52f3f
richknicks <rlynch@gmail.com>
2020-10-19 03:14:23 -0400

    installed tsparticles.js to background of App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,104 @@
 import React, { Component } from "react";
 import Navigation from "./Components/Navigation/Navigation";
 import Logo from "./Components/Logo/Logo";
+import ImageLinkForm from "./Components/ImageLinkForm/ImageLinkForm";
+import Rank from "./Components/Rank/Rank";
+import Particles from "react-tsparticles";
 import "./App.css";
+
+const particlesOptions = {
+  background: {
+    color: {
+      value: "red",
+    },
+  },
+  fpsLimit: 60,
+  interactivity: {
+    detectsOn: "canvas",
+    events: {
+      onClick: {
+        enable: true,
+        mode: "push",
+      },
+      onHover: {
+        enable: true,
+        mode: "repulse",
+      },
+      resize: true,
+    },
+    modes: {
+      bubble: {
+        distance: 400,
+        duration: 2,
+        opacity: 0.8,
+        size: 40,
+      },
+      push: {
+        quantity: 4,
+      },
+      repulse: {
+        distance: 200,
+        duration: 0.4,
+      },
+    },
+  },
+  particles: {
+    color: {
+      value: "#ffffff",
+    },
+    links: {
+      color: "#ffffff",
+      distance: 150,
+      enable: true,
+      opacity: 0.5,
+      width: 1,
+    },
+    collisions: {
+      enable: true,
+    },
+    move: {
+      direction: "none",
+      enable: true,
+      outMode: "bounce",
+      random: false,
+      speed: 7,
+      straight: false,
+    },
+    number: {
+      density: {
+        enable: true,
+        value_area: 800,
+      },
+      value: 80,
+    },
+    opacity: {
+      value: 0.5,
+    },
+    shape: {
+      type: "circle",
+    },
+    size: {
+      random: true,
+      value: 5,
+    },
+  },
+  detectRetina: true,
+};
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles
+          className="particles"
+          id="tsparticles"
+          options={particlesOptions}
+        />
         <Navigation />
         <Logo />
-        {/* <ImageLinkForm/>
-      <FaceRecognition/> */}
+        <Rank />
+        <ImageLinkForm />
+        {/* <FaceRecognition/> */}
       </div>
     );
   }
--- src/Components/ImageLinkForm/ImageLinkForm.js
+++ src/Components/ImageLinkForm/ImageLinkForm.js
@@ -2,7 +2,7 @@ import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
-// import "./ImageLinkForm.css";
+import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -31,11 +31,10 @@ const ImageLinkForm = () => {
   const classes = useStyles();
   return (
     <div className="container">
-      <p>
-        {
-          "The Face Recognition program will detect faces in your pictures. Give it a try!"
-        }
-      </p>
+      <Typography variant="h5">
+        "The Face Recognition program will detect faces in your pictures. Give
+        it a try!"
+      </Typography>
       <div className="inputBox">
         <form className={classes.root} noValidate autoComplete="off">
           <TextField
