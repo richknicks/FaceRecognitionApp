46825e97c0a20ff0c47f392a7082650470cc53a5
richknicks <rlynch@gmail.com>
2020-10-19 00:35:38 -0400

    Created Navigation Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
+import Navigation from "./Components/Navigation/Navigation";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <Navigation /> */}
+        <Navigation />
         {/* <Logo/>
       <ImageLinkForm/>
       <FaceRecognition/> */}
---
+++ src/Components/Navigation/Navigation.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Navigation = () => {
+  return (
+    <div>
+      <p>Navigation Page</p>
+    </div>
+  );
+};
+export default Navigation;
